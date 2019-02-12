2b14a16416e903bae3c750c7ea75fc1970c1d090
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-02-12 19:53:10 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'students', to: 'students#students'
+  get '/students', to: 'students#students'
 
-  get 'index', to: 'students#index'
+  get '/index', to: 'students#index'
 end
